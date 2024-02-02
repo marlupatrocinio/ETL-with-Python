edd11ba5c8d6d5ebf94768b87129396e392e6fda
Marlu <marlupatrocinio01@gmail.com>
2024-02-02 13:21:48 -0300

    Commit

--- etldemo.py
+++ etldemo.py
@@ -14,3 +14,29 @@ try:
 except Exception as e:
     print('Error reading config file: ' + str(e))
     sys.exit()
+    
+#read settings from config file
+startDate = config['CONFIG']['startDate']
+url = config['CONFIG']['url']
+destServer = config['CONFIG']['server']
+destDatabase = config['CONFIG']['database']
+
+#request data from url
+try:
+    BOCResponse = requests.get(url+startDate)
+except Exception as e:
+    print('Could not make request: ' + str(e))
+    sys.exit()
+
+#initialize list for data storage later
+BOCDates = []
+BOCRates = []
+
+#check response status code and process json
+if (BOCResponse.status_code == 200):
+    BOCRaw = json.loads(BOCResponse.text)
+ 
+    for row in BOCRaw['observations']:
+        BOCDates.append(datetime.datetime.strptime(row['d'], '%Y-%m-%d'))
+        BOCRates.append(decimal.Decimal(row['FXUSDCAD']['v']))
+        
